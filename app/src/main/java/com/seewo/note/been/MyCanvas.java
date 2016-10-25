d9dc41216c726ebaf9d65d425b7be15ee7f68cfb
xymobilesl <xymobilesl@163.com>
2016-10-25 15:19:21 +0800

    创建.gitignore

--- app/src/main/java/com/seewo/note/been/MyCanvas.java
+++ app/src/main/java/com/seewo/note/been/MyCanvas.java
@@ -3,7 +3,6 @@ package com.seewo.note.been;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
